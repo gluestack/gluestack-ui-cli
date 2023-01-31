a8be1c0b17a7fdee1cbb98308e575192402a776a
mayank-96 <pagarmayank07@gmail.com>
2023-01-31 15:59:45 +0530

    fix: removed initial prompts

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 #!/usr/bin/env node
 
-const prompts = require('prompts');
+// const prompts = require('prompts');
 const { componentAdder } = require('./component-adder');
 const { initChecker } = require('./init-checker');
 const { initializer } = require('./initializer');
 
 const main = async () => {
-  const commands = [
-    'initializer',
-    'init-checker',
-    'version manager',
-    'component adder',
-  ];
+  // const commands = [
+  //   'initializer',
+  //   'init-checker',
+  //   'version manager',
+  //   'component adder',
+  // ];
 
-  const actionResponse = await prompts({
-    type: 'select',
-    name: 'selectedOption',
-    message: 'Select task: ',
-    choices: commands,
-    initial: 0,
-  });
+  // const actionResponse = await prompts({
+  //   type: 'select',
+  //   name: 'selectedOption',
+  //   message: 'Select task: ',
+  //   choices: commands,
+  //   initial: 0,
+  // });
 
-  if (commands[actionResponse.selectedOption] === 'initializer') {
-    await initializer();
-  } else if (commands[actionResponse.selectedOption] === 'init-checker') {
-    initChecker();
-  } else if (commands[actionResponse.selectedOption] === 'component adder') {
+  // if (commands[actionResponse.selectedOption] === 'initializer') {
+  //   await initializer();
+  // } else if (commands[actionResponse.selectedOption] === 'init-checker') {
+  //   initChecker();
+  // } else if (commands[actionResponse.selectedOption] === 'component adder') {
+  //   await componentAdder();
+  // } else if (commands[actionResponse.selectedOption] === 'version manager') {
+  //   console.log('This feature is not available currently :(');
+  // }
+  if (initChecker()) {
     await componentAdder();
-  } else if (commands[actionResponse.selectedOption] === 'version manager') {
-    console.log('This feature is not available currently :(');
+  } else {
+    await initializer();
   }
 };
 
