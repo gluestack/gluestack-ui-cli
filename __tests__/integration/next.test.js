83dc3b054f32e9733ee421a6a154f3ba38b8b660
mayank-96 <pagarmayank07@gmail.com>
2023-04-17 15:56:59 +0530

    fix: test cases updated

--- __tests__/integration/next.test.js
+++ __tests__/integration/next.test.js
@@ -8,10 +8,9 @@ const fs = require('fs');
 const { promisify } = require('util');
 const request = require('supertest');
 
-
 const nextAppRootDirectory = join(__dirname, '../apps');
 const nextAppPath = join(__dirname, '../apps/my-next-app');
-const NEXT_PORT = "3039";
+const NEXT_PORT = '3039';
 const nextAppUrl = `http://localhost:${NEXT_PORT}`;
 
 describe('Next.js App Integration Test', () => {
@@ -19,33 +18,28 @@ describe('Next.js App Integration Test', () => {
   let initNextAppProcess;
   let nextServerStarted;
 
-
   const initGluestack = () => {
     return new Promise((resolve, reject) => {
+      const child = spawn('npx -y gluestack-ui@next init', {
+        shell: true,
+        cwd: nextAppPath, // Path to your Next.js app directory
+        // stdio: 'inherit',
+      });
 
-        const child = spawn('npx -y gluestack-ui@next init', {
-          shell: true,
-          cwd: nextAppPath, // Path to your Next.js app directory
-          // stdio: 'inherit',
-        });
-  
-        child.stdout.on('data', function (data) {
-          child.stdin.write('\n');
-          console.log(data.toString())
+      child.stdout.on('data', function (data) {
+        child.stdin.write('\n');
+        console.log(data.toString());
+      });
 
-        });
-  
-  
-        child.stdout.on('close', function () {
-          resolve(true);
-        });
-        child.stdout.on('error', function (error) {
-          console.log(error);
-          reject();
-        });
-    
+      child.stdout.on('close', function () {
+        resolve(true);
+      });
+      child.stdout.on('error', function (error) {
+        console.log(error);
+        reject();
+      });
     });
-  }
+  };
 
   const addComponent = () => {
     return new Promise((resolve, reject) => {
@@ -57,28 +51,26 @@ describe('Next.js App Integration Test', () => {
 
       child.stdout.on('data', function (data) {
         child.stdin.write('\n');
-        console.log(data.toString())
-
+        console.log(data.toString());
       });
-
 
       child.stdout.on('close', function () {
         resolve(true);
       });
       child.stdout.on('error', function (error) {
-        console.log(error)
+        console.log(error);
         reject();
       });
     });
-  }
-
-
+  };
 
   const updateNextjsConfiguration = () => {
     const documentPath = path.join(nextAppPath, 'src/app/__document.tsx');
     const nextConfig = path.join(nextAppPath, 'next.config.js');
 
-    fs.writeFileSync(documentPath, `import * as React from "react"
+    fs.writeFileSync(
+      documentPath,
+      `import * as React from "react"
       import { Html, Head, Main, NextScript } from "next/document"
       import { AppRegistry } from "react-native-web"
       import { flush } from "@dank-style/react"
@@ -103,10 +95,12 @@ describe('Next.js App Integration Test', () => {
         return { ...page, styles: React.Children.toArray(styles) }
       }
       
-      export default Document`);
+      export default Document`
+    );
 
-
-    fs.writeFileSync(nextConfig, `const { withGluestackUI } = require("@gluestack/ui-next-adapter")
+    fs.writeFileSync(
+      nextConfig,
+      `const { withGluestackUI } = require("@gluestack/ui-next-adapter")
 
       const nextConfig = {
         reactStrictMode: true,
@@ -114,82 +108,67 @@ describe('Next.js App Integration Test', () => {
           appDir: true,
         },
       }
-    module.exports = withGluestackUI(nextConfig)`);
-
-  }
-
-
+    module.exports = withGluestackUI(nextConfig)`
+    );
+  };
 
   beforeAll(async () => {
     try {
-       // Clean up existing app
-        spawnSync(`kill -9 $(lsof -t -i:${NEXT_PORT})`, {
+      // Clean up existing app
+      spawnSync(`kill -9 $(lsof -t -i:${NEXT_PORT})`, {
+        cwd: nextAppRootDirectory, // Path to your Next.js app directory
+        stdio: 'inherit',
+        shell: true,
+      });
+      spawnSync('rm -rf', ['my-next-app'], {
+        cwd: nextAppRootDirectory, // Path to your Next.js app directory
+        stdio: 'inherit',
+        shell: true,
+      });
+
+      // Start the Next.js app process
+
+      spawnSync(
+        'npx -y create-next-app@latest my-next-app --ts --eslint --src-dir false --experimental-app false --tailwind  --import-alias "@/*"',
+        {
           cwd: nextAppRootDirectory, // Path to your Next.js app directory
           stdio: 'inherit',
           shell: true,
-        })
-        spawnSync('rm -rf', ['my-next-app'], {
-          cwd: nextAppRootDirectory, // Path to your Next.js app directory
-          stdio: 'inherit',
-          shell: true,
-        })
-
-        // Start the Next.js app process
-
-        spawnSync('npx -y create-next-app@latest my-next-app --ts --eslint --src-dir false --experimental-app false --tailwind  --import-alias "@/*"', {
-          cwd: nextAppRootDirectory, // Path to your Next.js app directory
-          stdio: 'inherit',
-          shell: true,
-        });
-
-    } catch(Error) {
-      console.log(Error)
+        }
+      );
+    } catch (Error) {
+      console.log(Error);
     }
-   
-
   }, 60000);
-
-
-
 
   test('Init gluestack: Should exist gluestack-ui.config.ts', async () => {
     try {
       await initGluestack();
       const filePath = path.join(nextAppPath, 'gluestack-ui.config.ts');
       const fileExists = fs.existsSync(filePath);
-      expect(fileExists).toBe(true);
-    } catch(Error) {
+      expect(fileExists).toBeTruthy();
+    } catch (Error) {
       console.log(Error);
     }
-
-  }, 60000);
-
-
+  }, 70000);
 
   test('Init gluestack: Should exist components folder', () => {
     const filePath = path.join(nextAppPath, 'components');
 
-    console.log(filePath, "component file path")
+    console.log(filePath, 'component file path');
     const fileExists = fs.existsSync(filePath);
     expect(fileExists).toBe(true);
   });
 
-
-
   test('Update configuration: Should exist __document.tsx', async () => {
-
     await updateNextjsConfiguration();
 
     const filePath = path.join(nextAppPath, 'src/app/__document.tsx');
     const fileExists = fs.existsSync(filePath);
     expect(fileExists).toBe(true);
-
   }, 30000);
 
-
-
   test('Add component: Should exist button folder ', async () => {
-
     await addComponent();
 
     const filePath = path.join(nextAppPath, 'components/core/Button');
@@ -202,7 +181,6 @@ describe('Next.js App Integration Test', () => {
   }, 30000);
 
   test('Start Next.js app: Should render next app', async () => {
-
     appProcess = spawn(`yarn dev --port=${NEXT_PORT}`, {
       cwd: nextAppPath, // Path to your Next.js app directory
       // stdio: 'inherit',
@@ -219,24 +197,19 @@ describe('Next.js App Integration Test', () => {
 
       const match = data.toString().match(regex);
       if (match) {
-
         nextServerStarted = true;
-
       }
     });
 
-
     while (!nextServerStarted) {
       const output = await promisify(setTimeout)(1000);
-      console.log("Waiting for server to start")
+      console.log('Waiting for server to start');
     }
     // Send a GET request to the app URL
     const response = await request(nextAppUrl).get('/');
     //   // Verify that the response contains a component with text
     expect(response.text).toMatch(/Get started by editing/);
-
   }, 30000);
-
 
   // test('Usage button component: Should render button component', () => {
   //   expect(false).toMatch(true);
@@ -247,12 +220,10 @@ describe('Next.js App Integration Test', () => {
       appProcess.kill();
     }
     // Stop the app process
-    spawnSync('rm -rf', ['my-next-app'], {
+    spawnSync('rm -d', ['my-next-app'], {
       cwd: nextAppRootDirectory, // Path to your Next.js app directory
       stdio: 'inherit',
       shell: true,
-    })
+    });
   });
-
-
 });
