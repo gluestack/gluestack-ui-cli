b82f20ad8b4d59491c4e74cab8d08aded0b7f5aa
mayank-96 <pagarmayank07@gmail.com>
2023-05-02 15:45:43 +0530

    fix: updated help command

--- dist/index.js
+++ dist/index.js
@@ -34,21 +34,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
             const subCommand = process.argv[3];
             if (command === 'help') {
                 console.log(`
+
+    \x1b[36m- Init and add components\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest\x1b[0m
+
     \x1b[36m- Init gluestack-ui\x1b[0m
       \x1b[33mnpx gluestack-ui@latest init\x1b[0m
   
-    \x1b[36m- Init and add components\x1b[0m
-      \x1b[33mnpx gluestack-ui@latest\x1b[0m
-  
     \x1b[36m- Add component\x1b[0m
       \x1b[33mnpx gluestack-ui@latest add <component-name>\x1b[0m
+
+    \x1b[36m- Add all component\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest add --all\x1b[0m
   
     \x1b[36m- Update a component\x1b[0m
       \x1b[33mnpx gluestack-ui@latest update <component-name>\x1b[0m
   
+    \x1b[36m- Update all component\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest update --all\x1b[0m
+
     \x1b[36m- Remove a component\x1b[0m
       \x1b[33mnpx gluestack-ui@latest remove <component-name>\x1b[0m
   
+    \x1b[36m- Remove all component\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest remove --all\x1b[0m
+
     \x1b[36m- Help\x1b[0m
       \x1b[33mnpx gluestack-ui@latest help\x1b[0m`);
             }
--- src/index.ts
+++ src/index.ts
@@ -14,21 +14,31 @@ async function main() {
 
   if (command === 'help') {
     console.log(`
+
+    \x1b[36m- Init and add components\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest\x1b[0m
+
     \x1b[36m- Init gluestack-ui\x1b[0m
       \x1b[33mnpx gluestack-ui@latest init\x1b[0m
   
-    \x1b[36m- Init and add components\x1b[0m
-      \x1b[33mnpx gluestack-ui@latest\x1b[0m
-  
     \x1b[36m- Add component\x1b[0m
       \x1b[33mnpx gluestack-ui@latest add <component-name>\x1b[0m
+
+    \x1b[36m- Add all component\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest add --all\x1b[0m
   
     \x1b[36m- Update a component\x1b[0m
       \x1b[33mnpx gluestack-ui@latest update <component-name>\x1b[0m
   
+    \x1b[36m- Update all component\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest update --all\x1b[0m
+
     \x1b[36m- Remove a component\x1b[0m
       \x1b[33mnpx gluestack-ui@latest remove <component-name>\x1b[0m
   
+    \x1b[36m- Remove all component\x1b[0m
+      \x1b[33mnpx gluestack-ui@latest remove --all\x1b[0m
+
     \x1b[36m- Help\x1b[0m
       \x1b[33mnpx gluestack-ui@latest help\x1b[0m`);
   } else if (command === 'init') {
