b7a8ac047bebaac6fe5d71854c4c7013bb7bc272
Viraj-10 <virajjoshi132@gmail.com>
2023-07-13 15:48:37 +0530

    fix: version next app

--- packages/create-next-app-with-gluestack-ui/src/index.ts
+++ packages/create-next-app-with-gluestack-ui/src/index.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import minimist from 'minimist';
 const args = process.argv.slice(2);
 
--- packages/create-react-native-app-with-gluestack-ui/src/index.ts
+++ packages/create-react-native-app-with-gluestack-ui/src/index.ts
@@ -1,61 +1,83 @@
 #!/usr/bin/env node
 
+const args = process.argv.slice(2);
+let supportedArgs = ['--use-npm', '--use-yarn', '--help', '-h', '--use-pnpm'];
 import path from 'path';
 import fs from 'fs';
-import { cancel, isCancel, log, spinner, text } from '@clack/prompts';
+import {
+  cancel,
+  isCancel,
+  log,
+  multiselect,
+  spinner,
+  text,
+} from '@clack/prompts';
 import { select } from '@clack/prompts';
-import { execSync, spawnSync } from 'child_process';
+import { spawnSync } from 'child_process';
+import { installDependencies, getArgsData } from '@gluestack/cli-utils';
 
-function installDependencies(projectName: string) {
-  const projectPath = path.join(process.cwd(), projectName);
-  // try {
-  //   execSync(`cd ${projectPath} && yarn`);
-  //   console.log('Yarn command executed successfully.');
-  // } catch (error) {
-  //   console.error(`Error executing yarn: ${error}`);
-  // }
-
-  const s = spinner();
-  s.start('⏳ Installing dependencies...');
-
-  try {
-    spawnSync(
-      `yarn && git init && touch .gitignore   
-    echo "node_modules .expo" >> .gitignore`,
-      {
-        cwd: projectPath,
-        stdio: 'inherit',
-        shell: true,
-      }
-    );
-    s.stop(`\x1b[32mDependencies have been installed successfully.\x1b[0m`);
-  } catch (err) {
-    log.error(`\x1b[31mError: ${err}\x1b[0m`);
-    log.error('\x1b[31mError installing dependencies:\x1b[0m');
-    log.warning(` - Run \x1b[33m yarn \x1b[0m manually!`);
-    throw new Error('Error installing dependencies.');
-  }
-}
 async function main() {
-  let projectPath = path.join(path.resolve(__dirname, '..'), 'src', 'template');
+  let argsInfo = await getArgsData(args);
 
-  const projectName = await text({
-    message: 'What is the name of your application?',
-    placeholder: 'my-app',
-    defaultValue: 'my-app',
-    validate(value) {
-      if (value.length === 0) return `Value is required!`;
-    },
+  let projectName: any = argsInfo?.projectName;
+  let installationMethod: any = argsInfo?.installationMethod;
+  let projectPath = path.join(
+    path.resolve(__dirname, '..'),
+    'src',
+    'page-router'
+  );
+  if (projectName === '' || projectName == undefined) {
+    projectName = await text({
+      message: 'What is the name of your application?',
+      placeholder: 'my-app',
+      defaultValue: 'my-app',
+      // validate(value) {
+      //   if (value.length === 0) return `Value is required!`;
+      // },
+    });
+    if (isCancel(projectName)) {
+      cancel('Operation cancelled.');
+      process.exit(0);
+    }
+  }
+  const useAppRouter = await select({
+    message: 'Would you like to use App Router?',
+    options: [
+      {
+        value: 'yes',
+        label: 'yes',
+        hint: 'Next versions 13.4 and React server components support (recommended)',
+      },
+      {
+        value: 'no',
+        label: 'no',
+        hint: 'Next js page routing',
+      },
+    ],
   });
-  if (isCancel(projectName)) {
+  if (isCancel(useAppRouter)) {
     cancel('Operation cancelled.');
     process.exit(0);
   }
+  if (useAppRouter === 'yes') {
+    projectPath = path.join(path.resolve(__dirname, '..'), 'src', 'app-router');
+  }
+
   // copy directory
   const data = fs.cpSync(projectPath, path.join(process.cwd(), projectName), {
     recursive: true,
   });
-  installDependencies(projectName);
+  log.info(
+    ` Using \x1b[33m ${
+      installationMethod == undefined ? 'npm install' : installDependencies
+    } \x1b!`
+  );
+
+  installDependencies(
+    projectName,
+    // @ts-ignore
+    installationMethod == undefined ? 'npm install' : installDependencies
+  );
 }
 
 main();
