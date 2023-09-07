225b72c5ba9b2e926004f8755e96ec843e84394a
Vidhi Kataria <katariavidhi99@gmail.com>
2023-09-07 18:38:14 +0530

    pod install fixes

--- packages/create-react-native-app-with-gluestack-ui/src/index.ts
+++ packages/create-react-native-app-with-gluestack-ui/src/index.ts
@@ -6,8 +6,44 @@ let supportedArgs = ['--use-npm', '--use-yarn', '--help', '-h', '--use-pnpm'];
 
 import path from 'path';
 import fs from 'fs';
-import { cancel, isCancel, log, text } from '@clack/prompts';
-import { installDependencies, getArgsData } from '@gluestack/cli-utils';
+import { cancel, isCancel, log, text, spinner } from '@clack/prompts';
+import { getArgsData } from '@gluestack/cli-utils';
+import { spawnSync } from 'child_process';
+
+function installDependencies(projectName: string, installationMethod: string) {
+  const projectPath = path.join(process.cwd(), projectName);
+  const s = spinner();
+  s.start('⏳ Installing dependencies...');
+
+  try {
+    spawnSync(`mv gitignore .gitignore`, {
+      cwd: projectPath,
+      stdio: 'inherit',
+      shell: true,
+    });
+  } catch (err) {
+    log.error(`\x1b[31mError: gitignore file not found in template!\x1b[0m`);
+  }
+
+  try {
+    spawnSync(
+      `git init && ${installationMethod} && rm .npmignore && cd ios && pod install`,
+      {
+        cwd: projectPath,
+        stdio: 'inherit',
+        shell: true,
+      }
+    );
+
+    s.stop(`\x1b[32mDependencies have been installed successfully.\x1b[0m`);
+  } catch (err) {
+    log.error(`\x1b[31mError: ${err}\x1b[0m`);
+    log.error('\x1b[31mError installing dependencies:\x1b[0m');
+    log.warning(` - Run \x1b[33m yarn \x1b[0m manually!`);
+    log.warning(` - Run \x1b[33m pod install \x1b[0m manually!`);
+    throw new Error('Error installing dependencies.');
+  }
+}
 
 async function main() {
   let projectPath = path.join(path.resolve(__dirname, '..'), 'src', 'template');
