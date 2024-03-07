e6d668161acd2d03f10817bfc0d2c54427a49f81
Akash Gautam <akashgautam@geekyants.com>
2024-03-07 13:20:26 +0530

    chore: next page gui template

---
+++ apps/templates/next-page-router-with-gluestack-style/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '@/styles/globals.css';
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <GluestackUIProvider config={config}>
+      <Component {...pageProps} />
+    </GluestackUIProvider>
+  );
+}
