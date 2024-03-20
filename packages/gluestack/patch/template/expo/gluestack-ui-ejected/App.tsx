0917bf14f951b1232075460e932aec40f3e67774
Akash Gautam <akashgautam@geekyants.com>
2024-03-21 02:00:13 +0530

    fix: gui expo ejected template

--- packages/gluestack/patch/template/expo/gluestack-ui-ejected/App.tsx
+++ packages/gluestack/patch/template/expo/gluestack-ui-ejected/App.tsx
@@ -1,15 +1,16 @@
-import { config } from './config/gluestack-ui.config';
-import { Box, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { ScrollView } from 'react-native';
 import Gradient from './assets/Icons/Gradient';
 import DocumentData from './assets/Icons/DocumentData';
 import LightBulbPerson from './assets/Icons/LightbulbPerson';
 import Rocket from './assets/Icons/Rocket';
 import Logo from './assets/Icons/Logo';
+import { Box } from '@/components/ui/box';
+import { Text } from '@/components/ui/text';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 
 export default function App() {
   return (
-    <GluestackUIProvider config={config}>
+    <GluestackUIProvider>
       <Home />
     </GluestackUIProvider>
   );
