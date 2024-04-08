62b2ffa4c98a4eca000551c1f85448df7a65a218
Akash Gautam <akashgautam@geekyants.com>
2024-04-08 12:37:45 +0530

    fix: next page nw template flush fn

--- apps/templates/next-page-router-with-nativewind/pages/_document.tsx
+++ apps/templates/next-page-router-with-nativewind/pages/_document.tsx
@@ -1,8 +1,11 @@
+import * as React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
+import { AppRegistry } from "react-native-web";
+import { flush } from "@gluestack-ui/nativewind-utils/flush";
 
-export default function Document() {
+function Document() {
   return (
-    <Html lang="en">
+    <Html className="gs" lang="en">
       <Head />
       <body>
         <Main />
@@ -11,3 +14,13 @@ export default function Document() {
     </Html>
   );
 }
+
+Document.getInitialProps = async ({ renderPage }: any) => {
+  AppRegistry.registerComponent("Main", () => Main);
+  const { getStyleElement } = AppRegistry.getApplication("Main");
+  const page = await renderPage();
+  const styles = [getStyleElement(), flush()];
+  return { ...page, styles: React.Children.toArray(styles) };
+};
+
+export default Document;
