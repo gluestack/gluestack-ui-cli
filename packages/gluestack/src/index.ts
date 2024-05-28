634647b57b064dd6e3db9e7adf6d89bf2f6627fe
Akash Gautam <akash3gtm@gmail.com>
2024-05-28 13:31:38 +0530

    chore: mixed v1 and v2

--- packages/gluestack/src/index.ts
+++ packages/gluestack/src/index.ts
@@ -1,177 +1,33 @@
 #! /usr/bin/env node
-import path from 'path';
-import { cancel, text, select } from '@clack/prompts';
-import { execSync } from 'child_process';
-import { displayHelp } from './help';
-import templatesMap from './data.js';
-const { gitRepo, tag, options } = templatesMap;
-
-const args = process.argv.slice(2);
-
-const supportedFrameworkArgs = [
-  '--expo',
-  '--expo-router',
-  '--next-app-router',
-  '--next-page-router',
-  '--react-native',
-];
-
-const supportedStyleArgs = ['--gs', '--nw'];
-
-const supportedPackagemanagers = ['npm', 'yarn', 'pnpm', 'bun'];
-const supportedPackagemanagerArgs = supportedPackagemanagers.map(
-  (manager) => '--use-' + manager
-);
-
-const supportedDocumentationArgs = ['--help', '-h'];
-
-let supportedArgs = [
-  // frameworks
-  ...supportedFrameworkArgs,
-  // style options
-  ...supportedStyleArgs,
-  // package manager
-  ...supportedPackagemanagerArgs,
-  // documentation
-  ...supportedDocumentationArgs,
-];
-
-let selectedFramework = '';
-let selectedRouter = '';
-let selectedStyle = '';
-let selectedPackageManager = '';
-let projName = '';
-
-if (args.length > 0) {
-  if (args.some((arg) => supportedDocumentationArgs.includes(arg))) {
-    // trigger help commmand and exit.
-    displayHelp(options);
-  }
-
-  if (!args[0].startsWith('-')) {
-    projName = args[0];
-    args.shift();
-  }
-  args.forEach((arg) => {
-    if (supportedFrameworkArgs.includes(arg)) {
-      selectedFramework = arg.slice(2);
-    } else if (supportedStyleArgs.includes(arg)) {
-      selectedStyle = arg.slice(2);
-    } else if (supportedPackagemanagerArgs.includes(arg)) {
-      selectedPackageManager = arg.slice(6);
-    } else {
-      console.log(`Unsupported argument: ${arg}\n`);
-      displayHelp(options);
-    }
-  });
-}
-selectedStyle = 'nw';
+import { main as runLatest } from './v2';
+import { main as runLegacy } from './v1';
 
 async function main() {
-  process.on('SIGINT', function () {
-    cancel('Operation cancelled.');
-    process.exit(0);
-  });
-  let templateName = selectedFramework;
-  if (templateName === '') {
-    const { question, options: optionsType } = options.framework.default;
-    // @ts-ignore
-    selectedFramework = await select({
-      message: question,
-      options: [...optionsType],
-    });
-    templateName = selectedFramework;
-    if (selectedFramework !== 'react-native') {
-      const { question, options: optionsType } =
-        // @ts-ignore
-        options.framework.Route[selectedFramework];
-      // @ts-ignore
-      selectedRouter = await select({
-        message: question,
-        options: [...optionsType],
-      });
-      templateName = selectedRouter;
-    }
+  let args = process.argv.slice(2);
+
+  // Determine which version to run and remove the version argument
+  let version = 'nothing';
+  if (args.includes('--v1')) {
+    version = 'v1';
+    const index = args.indexOf('--v1');
+    args.splice(index, 1);
+  } else if (args.includes('--v2')) {
+    version = 'v2';
+    const index = args.indexOf('--v2');
+    args.splice(index, 1);
   }
 
-  if (projName === '') {
-    // @ts-ignore
-    projName = await text({
-      message: 'Enter the name of your project: ',
-      placeholder: 'my-app',
-      defaultValue: 'my-app',
-    });
+  // Update process.argv with the modified arguments
+  process.argv = [process.argv[0], process.argv[1], ...args];
+
+  // Now run the appropriate version
+
+  if (version === 'v1') {
+    await runLegacy();
+  } else if (version === 'v2') {
+    await runLatest();
+  } else {
+    await runLegacy();
   }
-
-  if (selectedStyle === '') {
-    const { question, options: optionsType } = options.style.default;
-    // @ts-ignore
-    selectedStyle = await select({
-      message: question,
-      options: [...optionsType],
-    });
-  }
-  templateName = `${templateName}-${selectedStyle}`;
-
-  if (selectedPackageManager === '') {
-    const userPackageManager = process.env.npm_config_user_agent;
-    if (userPackageManager && userPackageManager.includes('bun')) {
-      selectedPackageManager = 'bun';
-    } else if (userPackageManager && userPackageManager.includes('pnpm')) {
-      selectedPackageManager = 'pnpm';
-    } else if (userPackageManager && userPackageManager.includes('yarn')) {
-      selectedPackageManager = 'yarn';
-    } else {
-      selectedPackageManager = 'npm';
-    }
-  }
-
-  const templateDir = templatesMap.map[templateName];
-
-  // @ts-ignore
-  await cloneProject(projName, templateDir);
-
-  await installDependencies(projName, selectedPackageManager);
-  console.log('done ...');
 }
-
-async function cloneProject(projectName: string, templateName: string) {
-  const dirPath = path.join(process.cwd(), projectName);
-  // console.log(dirPath);
-  // console.log('Cloning Project...');
-  // try {
-  execSync(`mkdir ${projectName}`);
-  // } catch (error: any) {
-  //   console.log(`Folder already exists with name: ${projectName}`);
-  //   console.log('Overwriding the existing folder...');
-  //   execSync(`rm -rf ${projectName}`);
-  //   execSync(`mkdir ${projectName}`);
-  // }
-  execSync('git init', { cwd: dirPath });
-  execSync(`git remote add origin ${gitRepo}`, { cwd: dirPath });
-  execSync('git config core.sparseCheckout true', { cwd: dirPath });
-  execSync(
-    `echo "apps/templates/${templateName}" >> .git/info/sparse-checkout`,
-    {
-      cwd: dirPath,
-    }
-  );
-  execSync(`git pull origin ${tag}`, { cwd: dirPath });
-  execSync(`mv apps/templates/${templateName}/* ./`, { cwd: dirPath });
-  execSync('rm -rf apps', { cwd: dirPath });
-  execSync('rm -rf .git', { cwd: dirPath });
-  execSync('mv gitignore .gitignore', { cwd: dirPath });
-}
-
-async function installDependencies(
-  projectName: string,
-  selectedPackageManager: string
-) {
-  console.log('Installing Dependencies...');
-  execSync(`${selectedPackageManager} install`, {
-    cwd: path.join(process.cwd(), projectName),
-  });
-  console.log('Dependancies Installed!');
-}
-
 main();
