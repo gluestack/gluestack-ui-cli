1288c07a82652d08107644a64d66c4521ea58510
ankit-tailor <tailorankit3345@gmail.com>
2024-05-28 17:55:15 +0530

    fix: eject command

--- packages/eject-universal/src/index.ts
+++ packages/eject-universal/src/index.ts
@@ -8,46 +8,59 @@ export type { EjectConfig as Config } from "./script";
 
 async function main() {
   try {
-    const configPath = join(process.cwd(), "starter-cli.config.js");
+    const command = process.argv[2];
 
-    if (!existsSync(configPath)) {
-      console.error(
-        "Config file not found! Make sure you have starter-cli.config.js in your project root directory." +
-          "config path: " +
-          configPath
+    if (command === "eject") {
+      const configPath = join(
+        process.cwd(),
+        "gluestack-ui-universal.config.js"
       );
-      process.exit(1);
+
+      if (!existsSync(configPath)) {
+        console.error(
+          "Config file not found! Make sure you have gluestack-ui-universal.config.js in your project root directory." +
+            "config path: " +
+            configPath
+        );
+        process.exit(1);
+      }
+
+      console.info("Config file found! Reading config file...");
+
+      const { config } = require(configPath);
+
+      if (!config) {
+        const errorJSON = {
+          config: [
+            {
+              source: "path/to/source",
+              target: "path/to/target",
+            },
+          ],
+        };
+        throw new Error(
+          `Config not found in starter-cli.config.js! Please add\n module.exports = ${JSON.stringify(
+            errorJSON,
+            null,
+            2
+          )} \nto your config file.`
+        );
+      }
+
+      console.info("Config file read successfully! Copying directories...");
+
+      const ejectScript = new EjectScript(config);
+
+      await ejectScript.run();
+
+      console.info("Directories copied successfully!");
+    } else if (command === "--help") {
+      console.log("Usage: starter-cli eject");
+      console.log("Options:");
+      console.log("--help  Show help");
+      console.log("Commands:");
+      console.log("eject Eject universal starter kit");
     }
-
-    console.info("Config file found! Reading config file...");
-
-    const { config } = require(configPath);
-
-    if (!config) {
-      const errorJSON = {
-        config: [
-          {
-            source: "path/to/source",
-            target: "path/to/target",
-          },
-        ],
-      };
-      throw new Error(
-        `Config not found in starter-cli.config.js! Please add\n module.exports = ${JSON.stringify(
-          errorJSON,
-          null,
-          2
-        )} \nto your config file.`
-      );
-    }
-
-    console.info("Config file read successfully! Copying directories...");
-
-    const ejectScript = new EjectScript(config);
-
-    await ejectScript.run();
-
-    console.info("Directories copied successfully!");
     // copyDirectoriesFromSourceToTarget(config);
   } catch (err) {
     console.error("Error: ", err);
