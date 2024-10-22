d9affb8827ec4bd6c76d0cc3d8c25b62059a5046
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-10-22 14:53:41 +0530

    fix: next initializer

--- packages/gluestack-cli/template/codemods/nextjs/next-add-provider-transform.ts
+++ packages/gluestack-cli/template/codemods/nextjs/next-add-provider-transform.ts
@@ -1,5 +1,4 @@
 import { Transform } from 'jscodeshift';
-import { NextResolvedConfig } from '../../../src//util/config/config-types';
 
 const transform: Transform = (file, api, options) => {
   try {
--- packages/gluestack-cli/template/codemods/nextjs/next-config-js-transform.ts
+++ packages/gluestack-cli/template/codemods/nextjs/next-config-js-transform.ts
@@ -2,7 +2,9 @@ import { Transform } from 'jscodeshift';
 
 const transform: Transform = (file, api) => {
   try {
-    const j = api.jscodeshift;
+    const j = file.path.includes('.ts')
+      ? api.jscodeshift.withParser('ts')
+      : api.jscodeshift;
     const root = j(file.source);
 
     // Step 1: Check if the import statement for withGluestackUI already exists
