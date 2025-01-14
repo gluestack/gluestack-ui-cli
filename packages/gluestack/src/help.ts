89a729e756b6a94f81098cf9bd48bf9d2fde8b3e
Akash Gautam <akash3gtm@gmail.com>
2025-01-14 13:16:29 +0530

    fix: display args output

--- packages/gluestack/src/help.ts
+++ packages/gluestack/src/help.ts
@@ -1,30 +1,20 @@
-export function displayHelp(options: any) {
-  const frameworkOptions = options.framework;
-  const styleOptions = options.style.default.options;
+export function displayHelp() {
   console.log('Usage: create-gluestack [project-name] [options]');
   console.log('Options:\n');
   // framework options
   console.log('  framework options:');
-  const rnOptions = frameworkOptions.default.options[2];
-  console.log(`    --${rnOptions.value.padEnd(23)} ${rnOptions.hint}`);
-  frameworkOptions.Route.next.options.forEach((option: any) => {
-    console.log(`    --${option.value.padEnd(23)} ${option.hint}`);
-  });
-  frameworkOptions.Route.expo.options.forEach((option: any) => {
-    console.log(`    --${option.value.padEnd(23)} ${option.hint}`);
-  });
+  console.log(`--next-app-router         Next.js with app router
+    --expo-router             Expo app with Expo router V3
+    `);
   // styling options
   console.log('  style options:');
-  styleOptions.forEach((option: any) => {
-    console.log(
-      `    --${option.value.padEnd(23)} ${option.label}: ${option.hint}`
-    );
-  });
+  console.log(`--nw                      NativeWind: Tailwind styling for native
+    --gs                      gluestack-style: Universal and Performant Styling Library
+    `);
   // help options
   console.log('  help options:');
-  ['--help', '-h'].forEach((option) => {
-    console.log(`    ${option.padEnd(25)} show help`);
-  });
-
+  console.log(`--help                    show help
+    -h                        show help
+    `);
   process.exit(0);
 }
