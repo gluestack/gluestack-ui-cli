76e36dfa80e2e577401f30be306ccec5f38ba5fd
Akash Gautam <akash3gtm@gmail.com>
2025-01-14 13:20:47 +0530

    fix: help command

--- packages/gluestack/src/help.ts
+++ packages/gluestack/src/help.ts
@@ -3,18 +3,18 @@ export function displayHelp() {
   console.log('Options:\n');
   // framework options
   console.log('  framework options:');
-  console.log(`--next-app-router         Next.js with app router
+  console.log(`    --next-app-router         Next.js with app router
     --expo-router             Expo app with Expo router V3
     --universal               Universal app (Next.js with app router + Expo router)
     `);
   // styling options
   console.log('  style options:');
-  console.log(`--nw                      NativeWind: Tailwind styling for native
+  console.log(`    --nw                      NativeWind: Tailwind styling for native
     --gs                      gluestack-style: Universal and Performant Styling Library
     `);
   // help options
   console.log('  help options:');
-  console.log(`--help                    show help
+  console.log(`    --help                    show help
     -h                        show help
     `);
   process.exit(0);
