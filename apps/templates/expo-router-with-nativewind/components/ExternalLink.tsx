205f82e63282c51cddeae026ef03e9ad60965b12
Akash Gautam <akash3gtm@gmail.com>
2025-02-25 14:52:12 +0530

    fix: broken imports and ts-errors

--- apps/templates/expo-router-with-nativewind/components/ExternalLink.tsx
+++ apps/templates/expo-router-with-nativewind/components/ExternalLink.tsx
@@ -3,21 +3,21 @@ import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import { Platform } from "react-native";
 
-export function ExternalLink(
-  props: Omit<React.ComponentProps<typeof Link>, "href"> & { href: string }
-) {
+export function ExternalLink({
+  href,
+  ...props
+}: React.ComponentProps<typeof Link>) {
   return (
     <Link
       target="_blank"
       {...props}
-      // @ts-expect-error: External URLs are not typed.
-      href={props.href}
+      href={href}
       onPress={(e) => {
         if (Platform.OS !== "web") {
           // Prevent the default behavior of linking to the default browser on native.
           e.preventDefault();
           // Open the link in an in-app browser.
-          WebBrowser.openBrowserAsync(props.href as string);
+          WebBrowser.openBrowserAsync(href as string);
         }
       }}
     />
